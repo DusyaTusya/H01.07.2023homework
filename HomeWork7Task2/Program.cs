253f90755bf53218733ff8ef3565e2d775b46a13
Dusya <Dusya@gmail.com>
2023-07-01 20:02:49 +0300

    add homework

---
+++ HomeWork7Task2/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+// 0, 0 -> 1
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateArray2D(int rows, int columns, int min, int max)
+{
+    int[,] numbers = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return numbers;
+}
+
+void PrintArray2D(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            System.Console.Write($"{numbers[i,j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int SearchElement(int[,] array, int indexRows, int indexColumns)
+{
+    int element = array[0,0];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (i == indexRows)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                if (j == indexColumns)
+                {
+                    element = array[i,j];
+                    return element;
+                }
+            }
+        }
+    }return -1;
+}
+
+int[,] array = GenerateArray2D(3, 5, -10, 10);
+PrintArray2D(array);
+int indexRow = ReadInt("Введите индекс строки > ");
+int indexColumn = ReadInt("Введите индекс столбца > ");
+int searchElement = SearchElement(array, indexRow, indexColumn);
+
+if (searchElement == -1) 
+{
+    Console.WriteLine("В массиве нет числа с таким индексом");
+}
+else
+{
+    Console.WriteLine(searchElement);
+}
---
+++ HomeWork7Task3/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int ReadInt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray2D(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            System.Console.Write($"{numbers[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] GenerateArray2D(int rows, int columns)
+{
+    int[,] numbers = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            numbers[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return numbers;
+}
+
+double FindAvg(int[,] number)
+{
+    double sum = 0;
+    double avg = 0;   
+
+    for (int j = 0; j < number.GetLength(1); j++)
+    {
+        sum = 0;
+        avg = 0;
+        for (int i = 0; i < number.GetLength(0);i++)
+        {
+            sum += number[i,j];
+        }
+        avg = sum / number.GetLength(1);
+        System.Console.WriteLine($"Среднее арифметическое столбца № {j+1}: {avg:f2}");
+    }
+    return avg; 
+}
+
+int rows = ReadInt("введите количество строк: ");
+int columns = ReadInt("введите количество столбцов: ");
+int[,] newArray = GenerateArray2D(rows,columns);
+PrintArray2D(newArray);
+System.Console.WriteLine();
+double output = FindAvg(newArray);
+
